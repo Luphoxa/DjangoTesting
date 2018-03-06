b794cb5882652a339eddf1938c0e9582d1c63e7e
Irene Herrero <herreroi@in.tum.de>
2018-03-06 13:41:25 +0100

    Basic view returns minimal HTML

--- functional_tests.py
+++ functional_tests.py
@@ -12,7 +12,7 @@ browser_options.add_argument('--headless')
 browser_options.add_argument('--disable-gpu')
 
 using_firefox = True  # if false user uses chrome
-using_headless = True # if true, browser won't open up
+using_headless = False # if true, browser won't open up
 
 class NewVisitorTest(unittest.TestCase):
 
